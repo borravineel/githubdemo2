ff4c0ba0a0c43fc1871149bd1f5eb9c7978f706d
LENOVO <LENOVO@DESKTOP-C30NCKG>
2022-04-18 15:57:25 +0530

    firstcommit

---
+++ Githubdemo2/src/main/java/conditionalstatements/ConditionalStatements.java
@@ -0,0 +1,82 @@
+package conditionalstatements;
+
+public class ConditionalStatements {
+	public static void main(String[]args) {
+		//IF , ELSE STATEMENTS
+	//int i=65;
+	/*if(i>25) {
+	System.out.println("print i");
+	}
+	else {
+	System.out.println("print j");
+	*/
+//IF-ELSE-IF STATEMENTS
+	/*	int i=80;
+	
+		if(i<50) {
+			System.out.println("print i");
+}
+		else if(i>=50&&i<60){
+			System.out.println("a");
+	
+	}
+
+		else if(i>=60&&i<70){
+			System.out.println("print b");
+	}
+*/
+//		NESTED IF STATEMENTS
+/*	int a=30;
+	int b=60;
+	
+	if(a>=28) {
+		if(b>20) {
+			System.out.println("eligible");
+		}
+	}
+	*/
+	//SWITCHED STATEMENT
+	/*int i=10;
+	 switch(i) {
+	 case 10: System.out.println("print 10");
+	break;
+	 case 20:System.out.println("print 20");
+	// break;
+	 case 30:System.out.println("print 30");
+	 //break;
+	 }*/
+	 
+	//FOR LOOP
+
+	/* for(int i=1;i<=25;i++) {
+	 System .out.println(i);
+	 }
+	 */
+	 //NESTED FOR LOOP
+	 
+	 
+	 for (int i=1;i<=5;i++) {
+		 System.out.println(+i);
+	 for (int j=1;j<=5;j++) {
+		 System.out.println(+j);
+	 }
+	 }
+	 //FOR EACH LOOP
+	 
+	 
+	 }
+	 }
+	  
+	
+	
+	
+		
+	
+	
+
+
+		
+	
+	
+	
+		
---
+++ Githubdemo2/src/test/java/programs/programs.java
@@ -0,0 +1,46 @@
+package programs;
+
+public class programs {
+
+	public static void main(String[] args) {
+	
+//      REVERSE THE NUMBER
+	/*	int i=1234567;
+		int r=0;
+		while(i!=0) {
+			int remainder=i%10;
+			r=r*10+remainder;
+			i=i/10;
+		}
+		System.out.println(r);
+		
+		for(;i!=0;i=i/10) {
+			int remainder=i%10;
+			r=r*10+remainder;
+	}
+		System.out.println(r);
+	
+		//FIBONAIC SERIES
+		
+	int n1=1,n2=2,n3,i,count=12;
+		System.out.print(n1+" "+n2);
+		     for(i=3;i<count;++i) {
+			
+		n3=n1+n2;
+		System.out.print(" "+n3);
+		n1=n2;
+		n2=n3;
+		}
+	*/
+		//PYRAMID 	     
+		     
+		int term=6;  
+		for(int i=1;i<=term;i++){  
+		for(int j=term;j>=i;j--){  
+		        System.out.print("5");  
+		}  
+		System.out.println();//new line  
+		}  
+		}  
+}
+	
